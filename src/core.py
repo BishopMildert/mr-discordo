92bbcc9cbfe0307ab69ebdaad0379b14251b7de0
Gilly Hefler <ghefler@gmail.com>
2021-03-26 15:57:59 +0000

    first test passed

--- src/core.py
+++ src/core.py
@@ -1,1 +1,4 @@
 # core functionalities of bot go here
+import os
+
+print(os.getenv('B'))
--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 # main app features go here
 import discord
 import os
+from dotenv import load_dotenv
 
-client = discrod.Client()
+# load env credentials
+load_dotenv()
+
+client = discord.Client()
 
 @client.event
 async def on_ready():
@@ -13,7 +17,8 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello')
+    if message.content.endswith('Hamid says:'):
+        # call the function here to post a message into the server
+        await message.channel.send('no, fuck you!')
 
 client.run(os.getenv('TOKEN'))
